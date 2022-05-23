f4b44893a50c2581b1298cbb4c54900d9e6d0fc8
Khusniddin <khtkarimzhonov@gmail.com>
2022-05-23 12:45:39 +0300

    set telegram

--- telegram/loader.py
+++ telegram/loader.py
@@ -1,9 +1,22 @@
-from aiogram import types
-from aiogram import Bot
-from aiogram import Dispatcher
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram import types, Bot, Dispatcher
+from aiogram.dispatcher.filters.builtin import CommandStart
 from config import TELEGRAM_BOT_TOKEN
+from core.dialog import get_answer
 
 __all__ = ['dp']
 
-dp = Dispatcher(Bot(TELEGRAM_BOT_TOKEN, parse_mode=types.ParseMode.HTML), storage=MemoryStorage())
+dp = Dispatcher(Bot(TELEGRAM_BOT_TOKEN, parse_mode=types.ParseMode.HTML))
+
+
+async def commond_start(msg: types.Message):
+    text = 'This is bot for smart house'
+    await msg.answer(text)
+
+
+async def get_sentence_from_user(msg: types.Message):
+    answer = get_answer(msg.text)
+    await msg.answer(answer)
+
+
+dp.register_message_handler(commond_start, CommandStart())
+dp.register_message_handler(get_sentence_from_user)
